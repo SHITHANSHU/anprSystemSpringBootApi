e61dc81167315e3836c31ba9b2cf8aced9d92475
SHITHANSHU <shithanshu24121998@gmail.com>
2020-05-08 02:37:33 +0530

    resolved cors policy

--- src/main/java/com/anpr/demo/controller/loginController.java
+++ src/main/java/com/anpr/demo/controller/loginController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 //import org.apache.naming.java.javaURLContextFactory;
 //import org.apache.naming.java.javaURLContextFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,7 @@ import com.anpr.demo.repo.sessionRep;
 import com.anpr.demo.repo.tolldataRep;
 import com.anpr.demo.repo.tollrecordRep;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("anpr")
 public class loginController {
